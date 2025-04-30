0df26e5ca61c03e07eb971343b3e75b9d39290d0
MJMMiller <mateomiller1605@gmail.com>
2025-04-30 08:45:11 -0500

    FEAT(UPS-003): Benchmaking python

---
+++ src_phyton/app.py
@@ -0,0 +1,7 @@
+# import benchmarking as Ben
+from benchmarking import Benchmarking
+
+
+if __name__ == "__main__":
+    # Ben.Benchmarking()
+    Benchmarking()
---
+++ src_phyton/benchmarking.py
@@ -0,0 +1,37 @@
+import random
+import time
+from metodos_ordenamiento import MetodosOrdenamiento
+class Benchmarking:
+
+    def __init__(self):
+        print('Bench Inicializado')
+        self.mO = MetodosOrdenamiento()
+        arreglo = self.buil_arreglo(50000)
+
+        tarea = lambda: self.mO.sortByBubble(arreglo)
+        tiempoMillis = self.contar_con_current_time_milles(tarea)
+        tiempoNano = self.contar_con_nano_time(tarea)
+
+        print(f'Tiempo en Milis: {tiempoMillis}')
+        print(f'Tiempo en Nano: {tiempoNano}')
+
+    def buil_arreglo(self, tamanio):
+        array = []
+        for i in range (tamanio):
+            numero = random.randint(0, 99999)
+            array.append(numero)
+        return array
+    
+    def contar_con_current_time_milles(self, tarea):
+        inicio = time.time()
+        tarea()
+        fin = time.time()
+
+        return (fin-inicio)
+
+    def contar_con_nano_time(self, tarea):
+        inicio = time.time_ns()
+        tarea()
+        fin = time.time_ns()
+
+        return (fin-inicio)/1000000000.0
