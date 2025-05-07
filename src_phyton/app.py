b8142d8a0d2761828175e2e52ca1f59799efc750
MJMMiller <mateomiller1605@gmail.com>
2025-05-07 08:46:20 -0500

    FEAT(UPS-005): Implementacion de grafica

--- src_phyton/app.py
+++ src_phyton/app.py
@@ -1,6 +1,7 @@
 # import benchmarking as Ben
 from benchmarking import Benchmarking
 from metodos_ordenamiento import MetodosOrdenamiento
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
@@ -8,20 +9,45 @@ if __name__ == "__main__":
     metodosOrden = MetodosOrdenamiento()
     benchmark = Benchmarking()
 
-    tam = 10000
-    arreglo_base = benchmark.buil_arreglo(tam)
-
-    metodos = {"burbuja":metodosOrden.sortByBubble,
-               "seleccion":metodosOrden.sort_seleccion,
-    }
-    
+    tamanios = [500,1000,2000]
     resultados = []
 
-    for nombre, metodo in metodos.items():
-        tiempo = benchmark.medir_tiempo(metodo, arreglo_base)
-        tuplaResultado = (tam, nombre, tiempo)
-        resultados.append(tuplaResultado)
+    for tam in tamanios:
+        arreglo_base = benchmark.buil_arreglo(tam)
 
-    for resultado in resultados:
-        tam, nombre, tiempo = resultado
-        print (f"Tamano : {tam} || Metodo : {nombre} || Timpo : {tiempo:.6f} segunddos")
+        metodos = {
+            "burbuja":metodosOrden.sortByBubble,
+            "seleccion":metodosOrden.sort_seleccion,
+        }
+        
+        for nombre, metodo in metodos.items():
+            tiempo = benchmark.medir_tiempo(metodo, arreglo_base)
+            tuplaResultado = (tam, nombre, tiempo)
+            resultados.append(tuplaResultado)
+
+        for resultado in resultados:
+            tam, nombre, tiempo = resultado
+            print (f"Tamano : {tam} || Metodo : {nombre} || Tiempo : {tiempo:.6f} segundos")
+
+    tiempo_by_metodo = {
+        "burbuja" : [],
+        "seleccion" : []
+    }
+
+    #clasificar los tiempos segun el tiempo
+    for tam, nombre, tiempo in resultados:
+        tiempo_by_metodo[nombre].append(tiempo)
+    
+    #crear la grafica
+    plt.figure(figsize=(10,6))
+
+
+    for nombre, tiempo in tiempo_by_metodo.items():
+        plt.plot(tamanios, tiempo, label= nombre, marker='o')
+
+    #agregar parametros
+    plt.grid()
+    plt.suptitle('Comparativa metodos')
+    plt.ylabel('Tiempos obtenidos')
+    plt.xlabel('Tamaño del arreglo')
+    plt.show()
